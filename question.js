664fe9416a2749039db4a6151c3415817ae77331
quaidjohar23 <152946591+quaidjohar23@users.noreply.github.com>
2023-12-05 08:24:03 +0400

    Add files via upload

---
+++ question.js
@@ -0,0 +1,35 @@
+
+
+
+const quiz = [
+    {
+        q: 'Which month comes before june?',
+        options: ['may', 'september', 'july', 'april'],
+        answer: 0
+    },
+    {
+        q: 'What is your name?',
+        options: ['Tasneem', 'Quaid Joher', 'Shabbir', 'Huzefa'],
+        answer: 2
+    },
+    {
+        q: 'What is the name of your School?',
+        options: ['Diyafah', 'MSB', 'Pakistan', 'India'],
+        answer: 1
+    },
+    {
+        q: 'What is your teachers name?',
+        options: ['Rashida', 'Jamila', 'Munira', 'Fatema'],
+        answer: 3
+    },
+    {
+        q: 'What day comes after Saturday?',
+        options: ['Monday', 'wednesday', 'sunday', 'friday'],
+        answer: 2
+    },
+    {
+        q: 'What is the colour of Banana?',
+        options: ['Green', 'yeLLow', 'Orange', 'Black'],
+        answer: 1
+    },
+]
---
+++ question2.js
@@ -0,0 +1,35 @@
+
+
+
+const quiz = [
+    {
+        q: 'Which colour is ORange?',
+        options: ['orange', 'purple', 'red', 'yellow'],
+        answer: 0
+    },
+    {
+        q: 'What is your name?',
+        options: ['Tasneem', 'Quaid Joher', 'Shabbir', 'Huzefa'],
+        answer: 2
+    },
+    {
+        q: 'What is the name of your School?',
+        options: ['Diyafah', 'MSB', 'Pakistan', 'India'],
+        answer: 1
+    },
+    {
+        q: 'What is your teachers name?',
+        options: ['Rashida', 'Jamila', 'Munira', 'Fatema'],
+        answer: 3
+    },
+    {
+        q: 'What day comes after Saturday?',
+        options: ['Monday', 'wednesday', 'sunday', 'friday'],
+        answer: 2
+    },
+    {
+        q: 'What is the colour of Banana?',
+        options: ['Green', 'yeLLow', 'Orange', 'Black'],
+        answer: 1
+    },
+]
